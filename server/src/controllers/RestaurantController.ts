1dcadf612d9d0ff4ce1c76b3aea8654a69594258
Luis Saiete <luistsaiete@gmail.com>
2024-04-05 06:18:51 +0200

    Connect with DB

--- server/src/controllers/RestaurantController.ts
+++ server/src/controllers/RestaurantController.ts
@@ -8,12 +8,11 @@ export class RestaurantController {
     const { name, contact, slogan } = request.body;
     const id = randomUUID();
     this.repository.set(id, { name, contact, slogan });
-    return reply.send();
+    return reply.send({ status: 'success' });
   }
 
   list(request: FastifyRequest, reply: FastifyReply) {
     const restaurants = Array.from(this.repository);
-
     return reply.send(restaurants);
   }
 }
--- server/src/controllers/UserController.ts
+++ server/src/controllers/UserController.ts
@@ -1,13 +1,20 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
 import { randomUUID } from 'crypto';
+import { prisma } from '../db';
 
 export class UserController {
   users = new Map();
   restaurants = new Map();
-  create(request: FastifyRequest, reply: FastifyReply) {
+  async create(request: FastifyRequest, reply: FastifyReply) {
     const { name, email, password } = request.body;
-    const id = randomUUID();
-    this.users.set(id, { name, email, password });
+
+    await prisma.user.create({
+      data: {
+        name,
+        email,
+        password
+      }
+    });
     return reply.status(201).send();
   }
 }
