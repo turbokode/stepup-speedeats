efaa49a2acf74d739d52899ffad95e103a39106c
Luis Saiete <luistsaiete@gmail.com>
2024-06-06 00:38:48 +0200

    Errors handling

--- server/src/hooks/upload.ts
+++ server/src/hooks/upload.ts
@@ -3,11 +3,18 @@ import fs from 'fs';
 import { FastifyError, FastifyReply, FastifyRequest } from 'fastify';
 import path from 'path';
 import { UPLOADS_DIR } from '../utils/constants';
+import { z } from 'zod';
+interface Body {
+  [field: string]: object;
+}
 
 export function upload(field: string) {
   return (request: FastifyRequest, reply: FastifyReply, done: (err?: FastifyError) => void) => {
+    const SCh = z.object({
+      '*': z.object({})
+    });
+    const b = SCh.parse(request.body)
     const file = request.body[field];
-    console.log(UPLOADS_DIR);
 
     const fileName = `${randomUUID()}-${file.filename}`.replace(/ /g, '_');
     const filePath = path.join(UPLOADS_DIR, fileName);
